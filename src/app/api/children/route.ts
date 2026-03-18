6a94eb810ed4e689c848e98c32ecd99d61945e3e
João Marcos Alecsandro Kirimis <150600077+jmkirimis@users.noreply.github.com>
2026-03-18 10:38:21 -0300

    refactor: ajusta rotas para nomenclatura em inglês

--- src/app/api/children/route.ts
+++ src/app/api/children/route.ts
@@ -3,7 +3,7 @@ import { serverFetch } from '@/lib/serverFetch';
 
 export async function GET(req: NextRequest) {
 
-  const response = await serverFetch(`${process.env.API_URL}/pais/criancas`);
+  const response = await serverFetch(`${process.env.API_URL}/parents/children`);
 
   const result = await response.json();
   return NextResponse.json(result, { status: response.status });
@@ -12,7 +12,7 @@ export async function GET(req: NextRequest) {
 export async function POST(req: NextRequest) {
   const body = await req.json();
 
-  const response = await serverFetch(`${process.env.API_URL}/pais/criancas`, {
+  const response = await serverFetch(`${process.env.API_URL}/parents/children`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
