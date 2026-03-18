f9e2004e2c29beadd17041bfca3d68b86227f91a
João Marcos Alecsandro Kirimis <150600077+jmkirimis@users.noreply.github.com>
2026-03-18 15:57:41 -0300

    refactor: move types.ts para a pasta src e adiciona novos types

--- src/types.ts
+++ src/types.ts
@@ -1,28 +1,37 @@
 import { Dayjs } from "dayjs";
 
 export interface User {
-  _id: string;
+  _id?: string;
   profilePicture: string | null;
   username: string;
   name: string;
   email: string | null;
-  selectedChild: string;
+  selectedChild: string | null;
   type: "parent" | "child";
 };
 
-export type UserProfile = Omit<User, "id" | "selectedChild" | "type">;
+export type UserProfile = Omit<User, "_id" | "selectedChild" | "type">;
+
+export interface MedalItem {
+  _id: string;
+  name: string;
+  description: string;
+  date: string;
+}
 
 export interface Child {
-  _id: string;
+  _id?: string;
   profilePicture: string | null;
   username: string;
   name: string;
   points: number;
   audio: boolean | null;
   phasesCompleted: number | null;
-  medals: number | null;
+  medals: [MedalItem] | null;
   rankingActive: boolean | null;
 };
+
+export type ChildDataDashboard = Omit<Child, "_id" | "audio" | "rankingActive">;
 
 export type BodyRegisterType = {
   profilePicture: string | null;
