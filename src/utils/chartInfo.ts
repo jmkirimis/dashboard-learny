7408b8f55078894afb75e7169fb1836fe590e3d7
João Marcos Alecsandro Kirimis <150600077+jmkirimis@users.noreply.github.com>
2026-03-18 15:58:42 -0300

    feat: adiciona auxiliar para dados dos graficos

---
+++ src/utils/chartInfo.ts
@@ -0,0 +1,16 @@
+const barData = [
+  { name: "Seg", value: 20 },
+  { name: "Ter", value: 35 },
+  { name: "Qua", value: 50 },
+  { name: "Qui", value: 60 },
+];
+
+const pieData = [
+  { name: "Segunda", value: 25 },
+  { name: "Terça", value: 20 },
+  { name: "Quarta", value: 15 },
+  { name: "Quinta", value: 25 },
+  { name: "Sexta", value: 15 },
+];
+
+const CHART_COLORS = ["#F87171", "#60A5FA", "#34D399", "#FBBF24", "#FDE68A"];
